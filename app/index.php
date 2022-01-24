29c8c59ac4bb08f8a95bed96cb6ad1eb9b995258
Shoya-cyber <shoyas0627@gmail.com>
2022-01-24 15:28:46 +0900

    shopping cart 作成

--- app/index.php
+++ app/index.php
@@ -1,1 +1,76 @@
-hello
+<?php
+
+  //start session
+  session_start();
+
+  require_once('./php/CreateDb.php');
+  require_once('./php/component.php');
+
+  //create instance of Createdb class
+  $database = new CreateDb(dbname:"shopping_cart", tablename:"product");
+
+  if(isset($_POST['add'])){
+    // print_r($_POST['product_id']);
+    if(isset($_SESSION['cart'])){
+
+      $item_array_id = array_column($_SESSION['cart'], "product_id");
+      
+
+      if(in_array($_POST['product_id'], $item_array_id)){
+        echo "<script>alert('Product is already added in the cart...!')</script>";
+        echo "<script>window.location = 'index.php'</script>";
+      }else{
+        $count = count($_SESSION['cart']);
+        $item_array = array(
+          'product_id' => $_POST['product_id']
+        );
+        $_SESSION['cart'][$count] = $item_array;
+        print_r($_SESSION['cart']);
+      }
+
+    }else{
+      $item_array = array(
+        'product_id' => $_POST['product_id']
+      );
+
+      //create new session variable
+      $_SESSION['cart'][0] = $item_array;
+      print_r($_SESSION['cart']);
+    }
+
+
+  }
+  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Shopping Cart</title>
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- font awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <?php require_once('php/header.php'); ?>
+  <div class="container">
+    <div class="row text-center py-5">
+      <?php
+        $result = $database->getData();
+        while($row = mysqli_fetch_assoc($result)){
+          component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
+        }
+      ?>
+    </div>
+  </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2f1dca76656c22efc7241866e7d774fbdaa832af',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2f1dca76656c22efc7241866e7d774fbdaa832af',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v7.111.0',
+            'version' => '7.111.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => '374647b1079203ed7278ad4b58afc0fae3faaab9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
